c1b5ee7f823563c61574f9a5d56b74264fcf468a
Dimitris Kolotouros <dkolotouros@noc.grnet.gr>
2025-01-22 18:27:55 +0200

    Corrected code in deriveKey.c

--- decompiled/deriveKey.c
+++ decompiled/deriveKey.c
@@ -1,40 +1,32 @@
 
-void __cdecl decryptResource(byte *resource,dword size,byte *iv)
+undefined4 __cdecl deriveKey(char *passphrase,int passphraseSize,uint i)
 
 {
-  byte bVar1;
-  byte bVar2;
-  ulonglong uVar3;
-  byte bVar4;
-  byte bVar5;
-  uint uVar6;
-  uint uVar7;
-  int i;
+  ulonglong uVar1;
+  int j;
+  uint passIdx;
+  uint accumulator;
+  uint tmp;
   
+  tmp = i;
+  decryptionKey = (byte *)malloc(0x25e);
+  passIdx = 0;
+  j = 0;
+  do {
+    decryptionKey[j] = (byte)j;
+    j = j + 1;
+  } while (j < 0x25e);
   i = 0;
-  bVar4 = *iv;
-  bVar5 = iv[1];
-  if (0 < (int)size) {
-    uVar6 = (uint)bVar4;
-    uVar7 = (uint)bVar5;
-    do {
-      uVar3 = (ulonglong)(uVar6 + 1) % 0x25e;
-      uVar6 = (uint)uVar3 & 0xff;
-      bVar4 = (byte)uVar3;
-      uVar3 = (ulonglong)(decryptionKey[uVar6] + uVar7) % 0x25e;
-      uVar7 = (uint)uVar3 & 0xff;
-      bVar5 = (byte)uVar3;
-      swap(decryptionKey + uVar6,decryptionKey + uVar7);
-      bVar1 = decryptionKey[uVar7];
-      bVar2 = decryptionKey[uVar6];
-      ShowWindow((HWND)0x0,0);
-      resource[i] = resource[i] ^
-                    decryptionKey[(byte)((ulonglong)((uint)bVar1 + (uint)bVar2) % 0x25e)];
-      i = i + 1;
-    } while (i < (int)size);
-  }
-  *iv = bVar4;
-  iv[1] = bVar5;
-  return;
+  accumulator = 0;
+  *(undefined *)tmp = 0;
+  *(undefined *)(tmp + 1) = 0;
+  do {
+    accumulator = ((uint)decryptionKey[i] + accumulator + (byte)passphrase[passIdx & 0xff]) % 0x25e
+                  & 0xff;
+    swap(decryptionKey + i,decryptionKey + accumulator);
+    uVar1 = (ulonglong)((passIdx & 0xff) + 1);
+    passIdx = (uint)((longlong)uVar1 % (longlong)passphraseSize);
+    i = i + 1;
+  } while ((int)i < 0x25e);
+  return (int)((longlong)uVar1 / (longlong)passphraseSize);
 }
-
